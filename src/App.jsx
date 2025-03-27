25b2f3c78b2667e4945e9752e7f20ed77f032285
Minh Thanh <thanhhuynh0106@gmail.com>
2025-03-27 10:46:40 +0700

    first sight

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./components/navbar/navbar";
+import Home from "./components/home/home";
+import Search from "./components/search/Search";
+import Support from "./components/support/Support";
+import Info from "./components/info/Info";
+import Lounge from "./components/lounge/Lounge";
+import Travelers from "./components/travelers/Travelers";
+import Subcribers from "./components/subcribers/Subcribers";
+import Footer from "./components/footer/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+
+    </div>
+  );
+};
+
+
+export default App;
---
+++ src/components/subcribers/Subcribers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Subcribers = () => {
+  return (
+    <div>
+      Subcribers
+    </div>
+  )
+}
+
+export default Subcribers
+Subcribers
